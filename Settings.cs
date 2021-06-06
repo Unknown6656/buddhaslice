c136c1dbbb87e86f29e91ca86501cec8f0659008
unknown6656 <unknown84@gmx.de>
2021-06-06 17:41:28 +0200

    added settings types

---
+++ Settings.cs
@@ -0,0 +1,29 @@
+﻿
+namespace buddhaslice
+{
+    public sealed record Settings(
+        string path_mask,
+        string path_raw,
+        string path_out,
+        Bounds bounds,
+        int width,
+        int height,
+        int max_iter,
+        int max_image_size,
+        int slice,
+        int dpp,
+        int threads,
+        int cores,
+        double threshold_g,
+        double threshold_h,
+        bool claim_memory,
+        int report_interval_ms,
+        ExportSettings export
+    );
+
+    public sealed record Bounds(Bound image, Bound mask);
+
+    public sealed record Bound(double left, double right, double top, double bottom);
+
+    public sealed record ExportSettings(int interval_ms, bool raw, bool png, bool raw_at_end);
+}
