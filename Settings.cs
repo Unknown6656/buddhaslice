1a579d7c205b8e5e98808a586c53fe310520ec77
unknown6656 <unknown84@gmx.de>
2022-08-31 13:11:27 +0200

    switched to record structs

--- Settings.cs
+++ Settings.cs
@@ -8,67 +8,48 @@ using precision = System.Double;
 using precision = System.Single;
 #endif
 
-namespace buddhaslice
+namespace buddhaslice;
+
+
+public record struct Settings(
+    ulong width,
+    ulong height,
+    Bounds bounds,
+    Mask mask,
+    int max_iter,
+    int slice_offset,
+    int slice_count,
+    int dpp,
+    int cores,
+    precision threshold_g,
+    precision threshold_r,
+    precision scale_factor,
+    bool grayscale,
+    int report_interval_ms,
+    ExportSettings export
+)
 {
-    public struct Settings
-    {
-        public ulong width;
-        public ulong height;
-        public Bounds bounds;
-        public Mask mask;
-        public int max_iter;
-        public int slice_offset;
-        public int slice_count;
-        public int dpp;
-        public int cores;
-        public precision threshold_g;
-        public precision threshold_r;
-        public precision scale_factor;
-        public bool grayscale;
-        public int report_interval_ms;
-        public ExportSettings export;
-
-
-        public override string ToString() => JsonSerializer.Serialize(this, new JsonSerializerOptions { IncludeFields = true });
-    }
-
-    public struct Mask
-    {
-        public string path;
-        public Bounds bounds;
-    }
-
-    public struct Bounds
-    {
-        public precision left;
-        public precision right;
-        public precision top;
-        public precision bottom;
-
-        public precision Width => right - left;
-        public precision Height => bottom - top;
-
-
-        public Bounds(precision left, precision right, precision top, precision bottom)
-        {
-            this.left = left;
-            this.right = right;
-            this.top = top;
-            this.bottom = bottom;
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public bool Contains(precision x, precision y) => left <= x && x <= right && top <= y && y <= bottom;
-    }
-
-    public struct ExportSettings
-    {
-        public int interval_ms;
-        public bool raw;
-        public bool png;
-        public bool raw_at_end;
-        public string path_raw;
-        public string path_png;
-        public int max_image_size;
-    }
+    public override string ToString() => JsonSerializer.Serialize(this, new JsonSerializerOptions { IncludeFields = true });
 }
+
+public record struct Mask(string path, Bounds bounds);
+
+public record struct Bounds(precision left, precision right, precision top, precision bottom)
+{
+    public precision Width => right - left;
+    public precision Height => bottom - top;
+
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public bool Contains(precision x, precision y) => left <= x && x <= right && top <= y && y <= bottom;
+}
+
+public record struct ExportSettings(
+    int interval_ms,
+    bool raw,
+    bool png,
+    bool raw_at_end,
+    string path_raw,
+    string path_png,
+    int max_image_size
+);
