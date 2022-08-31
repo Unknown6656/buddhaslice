670190c322b66fddd35755ff18770e4e0a3568b7
unknown6656 <unknown84@gmx.de>
2022-08-31 13:07:37 +0200

    moved file

--- ImageTiler.cs
+++ ImageTiler.cs
@@ -1,6 +1,4 @@
-﻿#nullable enable
-
-using System.Runtime.CompilerServices;
+﻿using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using System.Drawing.Imaging;
 using System.Drawing;
